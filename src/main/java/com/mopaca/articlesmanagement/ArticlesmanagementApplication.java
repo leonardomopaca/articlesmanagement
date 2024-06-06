944ec08091ffd93ef86b3b6d18bcab094ece8109
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-06-06 10:28:59 -0500

    Project Ini

---
+++ src/main/java/com/mopaca/articlesmanagement/ArticlesmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.mopaca.articlesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mopaca/articlesmanagement/ArticlesmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mopaca.articlesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
